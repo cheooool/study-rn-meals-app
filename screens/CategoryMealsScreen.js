95ca1b330d1873f1033a8b0e2277ac0e9e2401f4
cheooool <g.cheooool@gmail.com>
2021-02-15 02:00:33 +0900

    MealItem 컴포넌트 추가 및 MealScreen에  MealItem 컴포넌트 추가

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,32 +1,40 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CaegoryMealsScreen = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {}}
+      />
+    );
+  };
+
   // 넘겨받은 파라메터를 가져올 수 있다.
   const catId = props.route.params.categoryId;
-  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
 
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) !== -1
+  );
   // 해당 페이지의 타이틀을 동적으로 변경하는 방법이다.
   // 또는 Stack.Screen의 options={ComponentName.navigationOptions}으로 사용 가능
   // props.navigation.setOptions({ title: selectedCategory.title });
 
   return (
     <View style={styles.screen}>
-      <Text>The Caegory Meals Screen</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => {
-          props.navigation.navigate('MealDetail');
-        }}
-      />
-      <Button
-        title="Go Back"
-        onPress={() => {
-          props.navigation.goBack();
-        }}
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: '100%' }}
       />
     </View>
   );
@@ -46,6 +54,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 10,
   },
 });
 
