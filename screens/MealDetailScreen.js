c059e81e9262211814cf0dc14788ae2785153070
cheooool <g.cheooool@gmail.com>
2021-02-15 22:50:38 +0900

    react-navigation-header-buttons 패키지 설치 및  favorite headerButton 컴포넌트 추가

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
+
 const MealDetailScreen = (props) => {
   const mealId = props.route.params.mealId;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
@@ -25,6 +29,17 @@ MealDetailScreen.navigationOptions = (props) => {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log('Mark as favorite!');
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
