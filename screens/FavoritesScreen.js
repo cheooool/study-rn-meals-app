db2a3fd8c7f0ee421d0a09fb4908db05d246acb1
cheooool <g.cheooool@gmail.com>
2021-02-16 23:28:07 +0900

    FavoritesScreen 컴포넌트 Fallback Text 추가

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = (props) => {
   const favMeals = useSelector((state) => state.meals.favoriteMeals);
 
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No Favorite Meals Found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default FavoritesScreen;
