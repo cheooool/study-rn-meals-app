7797967ff4740663d9f960be4ed7ffc05ca45d10
cheooool <g.cheooool@gmail.com>
2021-02-17 00:03:49 +0900

    Fallback text 추가

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
-import { CATEGORIES, MEALS } from '../data/dummy-data';
+import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const CaegoryMealsScreen = (props) => {
   // 넘겨받은 파라메터를 가져올 수 있다.
@@ -17,6 +19,13 @@ const CaegoryMealsScreen = (props) => {
   // 또는 Stack.Screen의 options={ComponentName.navigationOptions}으로 사용 가능
   // props.navigation.setOptions({ title: selectedCategory.title });
 
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found, maybe check your filters?</DefaultText>
+      </View>
+    );
+  }
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
@@ -28,5 +37,13 @@ CaegoryMealsScreen.navigationOptions = (props) => {
     headerTitle: selectedCategory.title,
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default CaegoryMealsScreen;
